91543f2db4a316305cc3186112e15c7137526852
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-12-09 22:08:35 +0700

    global style

---
+++ src/util/style_global.tsx
@@ -0,0 +1,87 @@
+import { StyleSheet } from 'react-native';
+
+const STYLE_GLOBAL = StyleSheet.create({
+    HEADER1: {
+        fontSize: 24,
+        fontFamily: 'Poppins-Medium'
+    },
+    HEADER2: {
+        fontSize: 18,
+        fontFamily: 'Poppins-Medium'
+    },
+    HEADER3: {
+        fontSize: 16,
+        fontFamily: 'Poppins-SemiBold'
+    },
+    PAGE: {
+        fontSize: 14,
+        fontFamily: 'Poppins-Regular'
+    },
+    SMALL: {
+        fontSize: 8,
+        fontFamily: 'Poppins-Medium'
+    },
+    BUTTON: {
+        fontSize: 18,
+        fontFamily: 'Poppins-SemiBold'
+    },
+    USER_TITTLE: {
+        fontSize: 15,
+        fontFamily: 'Poppins-Medium'
+    },
+    CARD_GRID: {
+        fontSize: 16,
+        fontFamily: 'Poppins-Medium'
+    },
+    MINI_BUTTON: {
+        fontSize: 12,
+        fontFamily: 'Poppins-Regular'
+    },
+    PRIMARI_COLOR: {
+        color: '#703B26'
+    },
+    SECONDARY_COLOR: {
+        color: '#A86741'
+    },
+    TERSIER_COLOR: {
+        color: '#D8B186'
+    },
+    BLACK_COLOR: {
+        color: '#000000'
+    },
+    GREEN_COLOR: {
+        color: '#3F7623'
+    },
+    GREEN_SEK_COLOR: {
+        color: '#649D33'
+    },
+    GREEN_TER_COLOR: {
+        color: '#26C165'
+    },
+    WHITE_COLOR: {
+        color: '#fff'
+    },
+    BACKGROUND_WHITE: {
+        backgroundColor: '#fff'
+    },
+    BACKGROUND_PREMIER: {
+        backgroundColor: '#703B26'
+    },
+    BACKGROUND_SEKUNDER: {
+        backgroundColor: '#A86741'
+    },
+    ENTER10: {
+        height: 10,
+    },
+    ENTER20: {
+        height: 20,
+    },
+    ENTER30: {
+        height: 30,
+    },
+    ENTER40: {
+        height: 40,
+    },
+});
+
+export default STYLE_GLOBAL
