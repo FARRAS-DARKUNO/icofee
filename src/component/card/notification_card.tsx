09084b317dcb599e1c8fa32b14bc5ace3396b8e3
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-12-31 13:34:40 +0700

    notification

--- src/component/card/notification_card.tsx
+++ src/component/card/notification_card.tsx
@@ -34,6 +34,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingHorizontal: 20,
+        marginBottom: 10,
 
     },
     card: {
--- src/page/notification.tsx
+++ src/page/notification.tsx
@@ -1,20 +1,43 @@
 import React from "react"
 import {
     Text,
-    StyleSheet
+    StyleSheet,
+    SafeAreaView,
+    View,
+    ScrollView
 } from "react-native"
+import STYLE_GLOBAL from "../util/style_global"
+import Header from "../component/header"
+import Card from "../component/card"
 
 const Notification = () => {
     return (
-        <Text> this Notification</Text>
+        <SafeAreaView style={[styles.container, STYLE_GLOBAL.BACKGROUND_WHITE]}>
+            <Header.HeaderTittle name="Pemberitahuan" />
+            <View style={STYLE_GLOBAL.ENTER20} />
+            <View style={styles.scroll}>
+                <ScrollView >
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                    <Card.NotificationCard tittle="Akun ansa sudah di ferivikasi dan alhamdullilah berhasil" time="20 Januari 2020" />
+                </ScrollView>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingVertical: 30,
     },
+    scroll: {
+        height: "82%"
+    }
 })
 
 export default Notification
